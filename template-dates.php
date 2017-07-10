2bceac1db3a702c823e45573c0d448f6081948cf
Mélissa Rissel <rissel.melissa@gmail.com>
2017-07-10 06:55:14 +0100

    dates table

--- template-dates.php
+++ template-dates.php
@@ -22,7 +22,7 @@
 	 ?>
 	<?php $events = get_posts(array(
 		'post_type'  => 'evenement',
-		'posts_per_page' => 2,
+		'posts_per_page' => -1,
 		'post_status' => 'publish',
 		'orderby' => 'date_start',
 		'order'=> 'ASC',
@@ -47,13 +47,21 @@
 		echo '<h2>' . ucfirst(utf8_encode(strftime('%B', mktime(null, null, null, $month, 1)))) . strftime(' %Y', mktime(null, null, null, null, null, $year + 1)) . '</h2>';
 	?>
 	<?php $i=1; ?>
-	<?php if ( $events ) : foreach ( $events as $post ) : setup_postdata( $post ); ?>
-		<?php $date = get_field('date_start', get_the_ID()  );  ?>
-		<?php $date_formatted =  date('d/m/Y', strtotime( $date )); ?>
-		<li><a href="<?php echo get_the_permalink(); ?>"><strong> <?php echo $date_formatted; ?></strong> <?php echo get_the_title(); ?></a></li>
-	<?php $i++; ?>
-	<?php endforeach; wp_reset_postdata(); endif; ?>
-	<?php if($i == 1) {echo '<p>Aucune date à retenir</p>';} ?>
+	<table class="events_table">
+		<?php if ( $events ) : foreach ( $events as $post ) : setup_postdata( $post ); ?>
+			<?php $id = get_the_ID() ; ?>
+			<?php $date = get_field('date_start', $id );  ?>
+			<?php $date_formatted =  date('d M Y', strtotime( $date )); ?>
+			<tr>
+				<td><strong><?php echo get_the_title(); ?></strong><td>
+				<td><?php echo $date_formatted; ?><td>
+				<td><?php echo get_field('time', $id); ?> </td>
+				<td><?php echo get_field('place', $id); ?> </td>
+			</tr>
+		<?php $i++; ?>
+		<?php endforeach; wp_reset_postdata(); endif; ?>
+		<?php if($i == 1) {echo '<p>Aucune date à retenir</p>';} ?>
+	</table>
 </div>
 <?php get_template_part('sidebar-date'); ?>
 <div style="clear:both"></div>
