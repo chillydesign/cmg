77d2fe860d10257fa0d1ace2efc880717f27526c
Charles Harvey <harvey.charles@gmail.com>
2022-08-26 11:43:38 +0100

    remove css from gitignore

---
+++ js/min/tiva_fr-debug.js
@@ -0,0 +1,23 @@
+// Day, Month
+var wordMonth = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+
+var wordDay_mon = "Lundi";
+var wordDay_tue = "Mardi";
+var wordDay_wed = "Mercredi";
+var wordDay_thu = "Jeudi";
+var wordDay_fri = "Vendredi";
+var wordDay_sat = "Samedi";
+var wordDay_sun = "Dimanche";
+
+
+
+// View Button
+var calendar_view = "Calendrier"
+var list_view = "Liste"
+var back = "Retour"
+
+// Calendar Button
+var prev_year = "Année précédente"
+var prev_month = "Mois précédent"
+var next_month = "Mois suivant"
+var next_year = "Année prochaine"
