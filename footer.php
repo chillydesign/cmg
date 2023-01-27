3b195541cdb83e7a6d8f94d4e12031a3ccbe53f4
Charles Harvey <harvey.charles@gmail.com>
2023-01-27 10:38:50 +0000

    add export for personnel

--- footer.php
+++ footer.php
@@ -1,58 +1,88 @@
+<!-- footer -->
+<footer class="footer" role="contentinfo">
 
+	<p class="">
+		&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. Website by <a href="//webfactor.ch" title="Webfactor">Webfactor</a>.
+	</p>
 
-	<!-- footer -->
-	<footer class="footer" role="contentinfo">
-
-		<p class="">
-			&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. Website by <a href="//webfactor.ch" title="Webfactor">Webfactor</a>.
-		</p>
-
-	</footer>
-	<!-- /footer -->
+</footer>
+<!-- /footer -->
 
 
 
- </div> <!-- END OF  page_container -->
+</div> <!-- END OF  page_container -->
 
 
 <?php get_sidebar(); ?>
 
- </div> <!-- END OF  page_section -->
+</div> <!-- END OF  page_section -->
 
 
+<?php
 
+if (isset($_GET['asdfhjk'])) {
+
+	$tags = get_terms([
+		'taxonomy'  => 'personnel_category',
+		'hide_empty'    => false
+	]);
+	$personnel = get_posts(array('posts_per_page' => -1, 'post_type' => 'personnel'));
+
+	foreach ($personnel as $p) {
+		$f = array();
+		array_push($f, $p->post_title);
+		array_push($f,  get_field('position', $p->ID));
+		array_push($f,  get_field('telephone_1', $p->ID));
+		array_push($f,  get_field('telephone_2', $p->ID));
+		array_push($f,  get_field('email', $p->ID));
+		array_push($f,  get_field('addresse', $p->ID));
+
+		foreach ($tags as $t) {
+			array_push($f,   in_category($t->term_id, $p->ID));
+		}
+
+
+		echo implode(',', $f);
+		echo "\n";
+	};
+}
+
+?>
 <div id="sidebar_bg"></div>
 
 
-		<?php $td = get_template_directory_uri() ; ?>
+<?php $td = get_template_directory_uri(); ?>
 
-        <script type='text/javascript' src='<?php echo home_url(); ?>/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/theia-sticky-sidebar/dist/ResizeSensor.min.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/theia-sticky-sidebar/dist/theia-sticky-sidebar.min.js"></script>
-		<!-- <script type="text/javascript" src="<?php // echo $td; ?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
-        <!-- <script type="text/javascript" src="<?php // echo $td; ?>/bower_components/moment/min/moment.min.js"></script> -->
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/underscore/underscore-min.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.min.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.gallery.min.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/bower_components/bxslider-4/dist/jquery.bxslider.min.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/js/tiva_fr.js"></script>
-		<script type="text/javascript" src="<?php echo $td; ?>/js/tiva.js"></script>
-		<!-- <script type="text/javascript" src="<?php echo $td; ?>/js/min/scripts.js"></script> -->
-		<script type="text/javascript" src="<?php echo $td; ?>/js/scripts.js"></script>
-        <link rel="stylesheet" href="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.min.css">
-        <link rel="stylesheet" href="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.gallery.min.css">
-		<?php wp_footer(); ?>
+<script type='text/javascript' src='<?php echo home_url(); ?>/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/theia-sticky-sidebar/dist/ResizeSensor.min.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/theia-sticky-sidebar/dist/theia-sticky-sidebar.min.js"></script>
+<!-- <script type="text/javascript" src="<?php // echo $td; 
+											?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
+<!-- <script type="text/javascript" src="<?php // echo $td; 
+											?>/bower_components/moment/min/moment.min.js"></script> -->
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/underscore/underscore-min.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.min.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.gallery.min.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/bower_components/bxslider-4/dist/jquery.bxslider.min.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/js/tiva_fr.js"></script>
+<script type="text/javascript" src="<?php echo $td; ?>/js/tiva.js"></script>
+<!-- <script type="text/javascript" src="<?php echo $td; ?>/js/min/scripts.js"></script> -->
+<script type="text/javascript" src="<?php echo $td; ?>/js/scripts.js"></script>
+<link rel="stylesheet" href="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.min.css">
+<link rel="stylesheet" href="<?php echo $td; ?>/bower_components/featherlight/release/featherlight.gallery.min.css">
+<?php wp_footer(); ?>
 
 
 
-		<script>
-		// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		// ga('send', 'pageview');
-		</script>
+<script>
+	// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+	// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+	// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+	// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+	// ga('send', 'pageview');
+</script>
 
-	</body>
+</body>
+
 </html>
